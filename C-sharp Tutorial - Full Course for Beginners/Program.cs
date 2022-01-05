46b44d00dba46706990c5576ede071b888a73c90
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-05 10:47:47 +0100

    Add project files.

---
+++ C-sharp Tutorial - Full Course for Beginners/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_sharp_Tutorial___Full_Course_for_Beginners
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was 35 years " + characterAge);
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But diden't like being " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
